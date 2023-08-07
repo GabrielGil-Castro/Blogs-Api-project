9f859f578472c6ac1e233e2ec846b78124816cfc
GabrielGil-Castro <gsgc05@gmail.com>
2023-08-07 18:54:32 -0300

    cria model Category

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+const CategoryModel = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    }, {
+      timestamps: false,
+      underscored: true,
+  });
+    return Category;
+  };
+  
+  module.exports = CategoryModel;
