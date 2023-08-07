3f9e279de031a091e07617d0dc01348d124afe7b
GabrielGil-Castro <gsgc05@gmail.com>
2023-08-07 18:59:52 -0300

    Cria model PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,46 @@
+const PostCategoryModel = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory', {
+      postId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        field: 'post_id',
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      categoryId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        field: 'category_id',
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+    }, {
+      timestamps: false,
+      tableName: 'posts_categories',
+      underscored: true,
+    });
+  
+    PostCategory.associate = (models) => {
+      models.Category.belongsToMany(models.BlogPost, {
+        through: PostCategory, foreignKey: 'categoryId', otherKey: 'postId'
+      });
+  
+      models.BlogPost.belongsToMany(models.Category, {
+        through: PostCategory, foreignKey: 'postId', otherKey: 'categoryId', as: 'categories'
+      });
+    };
+  
+    return PostCategory;
+  };
+  
+  module.exports = PostCategoryModel;
